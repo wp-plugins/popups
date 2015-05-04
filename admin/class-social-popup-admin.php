2d6301564d0ac62fa92b66f32cec1b5b891e5c79
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 15:41:59 +0000

    tagging 1.3.0.1 hotfix
    git-svn-id: https://plugins.svn.wordpress.org/popups/trunk@1153021 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/class-social-popup-admin.php
+++ admin/class-social-popup-admin.php
@@ -687,6 +687,10 @@ class SocialPopup_Admin {
 	 * @since 1.2.3.6
 	 */
 	function editor_styles() {
-		add_editor_style( SPU_PLUGIN_URL .'admin/assets/css/editor-style.css' );
+		$post_type = isset($_GET['post']) ? get_post_type($_GET['post']) : '';
+
+		if( 'spucpt' == $post_type || get_post_type() == 'spucpt' || (isset( $_GET['post_type']) && $_GET['post_type'] == 'spucpt') ) {
+			add_editor_style( SPU_PLUGIN_URL . 'admin/assets/css/editor-style.css' );
+		}
 	}
 }
